7e53842644730c9cc9a8d413302eb00b02ffadd4
Derek Liu (Adm) <adminliuderek@utorcsi.utoronto.ca>
2017-06-15 12:01:25 -0400

    Edited the comment-based help

--- Install-Win10x64.ps1
+++ Install-Win10x64.ps1
@@ -5,9 +5,15 @@
 Author:
 Date:
 
+.Synopsis 
+Installs IP printer and configures port for Windows 10 desktops.
+
+.Description
+This is the "variables" script. This script dot-sources the separate "logic and functions" script.
+Only this script needs to be edited for each printer installation.
+
 .Notes
 Input printer installation parameters below, retaining all quotations marks as is.
-
 The $PSScriptRoot automatic variable expands to the path of the install script itself. The driver folder and .inf file is relative to this value. 
 #>
 
