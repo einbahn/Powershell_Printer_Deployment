3ba0e49948cdd1fe8763bfb8c41048e97e862afb
Derek Liu (Adm) <adminliuderek@utorcsi.utoronto.ca>
2017-06-15 11:58:16 -0400

    substituted line breaks for semicolons, revised broken path for Install-IPPrinter.ps1

--- Install-Win10x64.ps1
+++ Install-Win10x64.ps1
@@ -5,23 +5,29 @@
 Author:
 Date:
 
-.Synopsis
-Configure port and install printer for direct IP printing on Windows 10. For detailed instructions, see README.txt in the same folder.
+.Notes
+Input printer installation parameters below, retaining all quotations marks as is.
+
+The $PSScriptRoot automatic variable expands to the path of the install script itself. The driver folder and .inf file is relative to this value. 
 #>
 
 #define variables
 [hashtable]$installparms = 
 
     @{
-    PrinterName         = 'ITS_Ricoh_MPC_3002_1 (UTORcsi)';
-    PrinterHostAddress  = '142.1.209.215';
-    DriverName          = 'RICOH Aficio MP C3002 PCL 6';
-    Location            = '246 Bloor, 6th Floor';
+    PrinterName         = 'ITS_Ricoh_MPC_3002_1 (UTORcsi)'
+
+    PrinterHostAddress  = '142.1.209.215'
+
+    DriverName          = 'RICOH Aficio MP C3002 PCL 6'
+
+    Location            = '246 Bloor, 6th Floor'
+
     InfPath             = "$PSScriptRoot\PCL6-WIN7x64\OEMSETUP.inf"
     }
 
 #dot-source the printer install function
-. '\\forum1\dsl$\printers\Powershell Printer Deployment\Install-IPPrinter.ps1'
+. '\\forum1\dsl$\printers\Powershell_Printer_Deployment\Install-IPPrinter.ps1'
 
 #call printer install function
 Install-IPPrinter @installparms
